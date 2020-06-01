faa13ae872949f6520446796604fb7ec50b9a276
leandro-gft <lpsacchi@gmail.com>
2020-06-01 12:37:05 -0300

    Adicionados os elementos de interface da aba Bichos

--- app/src/main/java/br/com/gft/aprendaingles/activity/MainActivity.java
+++ app/src/main/java/br/com/gft/aprendaingles/activity/MainActivity.java
@@ -1,16 +1,47 @@
 package br.com.gft.aprendaingles.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
 import br.com.gft.aprendaingles.R;
+import br.com.gft.aprendaingles.fragment.AnimaisFragment;
+import br.com.gft.aprendaingles.fragment.NumerosFragment;
+import br.com.gft.aprendaingles.fragment.VogaisFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setElevation(0);
+        getSupportActionBar().setTitle("Aprenda Inglês");
+
+        smartTabLayout = findViewById(R.id.viewPagerTab);
+        viewPager = findViewById(R.id.viewPager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                .add("Bichos", AnimaisFragment.class)
+                .add("Números", NumerosFragment.class)
+                .add("Vogais", VogaisFragment.class).
+                        create()
+                );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
     }
 }
